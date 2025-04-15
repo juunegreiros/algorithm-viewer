b9386ddfcb3f528ca5764b66cbc5f812925e7957
Juliana Negreiros <ju_bsnegreiros@hotmail.com>
2025-04-15 00:31:27 +0000

    3 Estilos iniciais

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,7 @@
-import { useState } from "react";
-
-const Button = ({ onClick, children, isDisable, color }) => {
-  return (
-    <button
-      className="banana"
-      style={{
-        background: color,
-        color: "white",
-      }}
-      onClick={onClick}
-      disabled={isDisable}
-    >
-      {children}
-    </button>
-  );
-};
+import "./App.scss";
 
 function App() {
-  const [count, setCount] = useState(0);
-  return (
-    <div>
-      <Button color="red" onClick={() => setCount(count + 1)}>
-        +
-      </Button>
-      <p>{count}</p>
-      <Button
-        color="green"
-        onClick={() => setCount(count - 1)}
-        isDisable={count <= 0}
-      >
-        -
-      </Button>
-    </div>
-  );
+  return <div className="app">oi</div>;
 }
 
 export default App;
